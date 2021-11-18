de13331e9d7b2e71c4198d5b00b5e1a18215563c
Leonardo Isso <leoisso.work@gmail.com>
2021-11-18 18:17:20 -0300

    feat: add missing parameters test

--- packages/server/src/views/create-user/__test__/create-user.spec.js
+++ packages/server/src/views/create-user/__test__/create-user.spec.js
@@ -21,13 +21,27 @@ describe('Views', () => {
 
     it('Should try to create the same user again, and return error', async () => {
       const result = await createUser(user)
-      expect(result).toHaveProperty('error')
+      expect(result).toHaveProperty('error', 'Email já está cadastrado')
     })
 
     it('Should try to create the an user without email, and return error', async () => {
       const { email, ...brokenUser } = user
       const result = await createUser(brokenUser)
-      expect(result).toHaveProperty('error')
+      expect(result).toHaveProperty('error', 'O campo email é obrigatório')
+    })
+
+    it('Should try to create the an user without password, and return error', async () => {
+      const { password, ...brokenUser } = user
+      const email = `${Math.random().toString(36).substring(2, 15)}@gmail.com`
+      const result = await createUser({ ...brokenUser, email })
+      expect(result).toHaveProperty('error', 'O campo password é obrigatório')
+    })
+
+    it('Should try to create the an user without name, and return error', async () => {
+      const { name, ...brokenUser } = user
+      const email = `${Math.random().toString(36).substring(2, 15)}@gmail.com`
+      const result = await createUser({ ...brokenUser, email })
+      expect(result).toHaveProperty('error', 'O campo name é obrigatório')
     })
   })
 })
