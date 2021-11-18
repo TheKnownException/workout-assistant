aae884b0eed4c35449eb29a902d151b02842d2cf
Leonardo Isso <leoisso.work@gmail.com>
2021-11-18 18:17:05 -0300

    feat: add error handling rules to view

--- packages/server/src/views/create-user/create-user.js
+++ packages/server/src/views/create-user/create-user.js
@@ -2,14 +2,6 @@ import user from '../../domains/user'
 import error from '../../domains/error'
 import { logError } from '../../infra/log'
 
-const catchError = err => {
-  logError(err)
-  if (err.code === 11000) {
-    return { error: error.constants.EMAIL.ALREADY_EXISTS }
-  }
-  return { error: err.message }
-}
-
 const createUser = async body => {
   try {
     const role = user.constants.ROLES.user
@@ -18,7 +10,8 @@ const createUser = async body => {
     const newUser = await user.controller.create(userData)
     return newUser
   } catch (err) {
-    return catchError(err)
+    logError(err)
+    return error.rules.defaultCreateValidations(err)
   }
 }
 
