c4871afa7de9666bfdbf053051cc707fb9f84344
Leonardo Isso <leoisso.work@gmail.com>
2021-11-19 03:39:35 -0300

    feat: add token to user Model

--- packages/server/src/domains/user/model.js
+++ packages/server/src/domains/user/model.js
@@ -3,6 +3,7 @@ import { Schema, model } from 'mongoose'
 const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, select: false },
+  token: { type: String, select: false },
   name: { type: String, required: true },
   role: { type: String, required: true },
   isActive: { type: Boolean, default: true },
