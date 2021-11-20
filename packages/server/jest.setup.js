cdd9cf92acbc5547957f53682aedf4856a675369
Leonardo Isso <leoisso.work@gmail.com>
2021-11-20 04:36:31 -0300

    feat: improve connection handler

--- packages/server/jest.setup.js
+++ packages/server/jest.setup.js
@@ -5,9 +5,16 @@ dotenv.config()
 jest.setTimeout(30000)
 
 beforeAll(async () => {
-  mongoose.connect(process.env.MONGODB_URI_TESTS, () => {
-    console.log('Connected to Mongo')
-  })
+  mongoose
+    .connect(process.env.MONGODB_URI_TESTS)
+    .then(x => {
+      console.log(
+        `Connected to Mongo! Database name: "${x.connections[0].name}"`
+      )
+    })
+    .catch(err => {
+      console.error('Error connecting to mongo', err)
+    })
 })
 
 afterAll(async () => {
