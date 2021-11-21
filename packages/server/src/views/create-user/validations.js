07bfc1fbfff86b2aa423fd31d7b17097ab1998df
Leonardo Isso <leoisso.work@gmail.com>
2021-11-21 05:25:09 -0300

    feat: use case validation throws custom error

--- packages/server/src/views/create-user/validations.js
+++ packages/server/src/views/create-user/validations.js
@@ -1,4 +1,5 @@
 import Joi from 'joi'
+import { ValidationError } from '../../domains/error/errors'
 
 const schema = Joi.object().keys({
   email: Joi.string().email().required(),
@@ -12,7 +13,8 @@ const schema = Joi.object().keys({
 const inputValidation = body => {
   const joiValidation = schema.validate(body)
   if (joiValidation.error) {
-    throw new Error(joiValidation.error.details[0])
+    const errorDetails = joiValidation.error.details[0]
+    throw new ValidationError(errorDetails.message, errorDetails.context.key)
   }
 }
 
