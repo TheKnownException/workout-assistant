435bb753afe34588d5b9ab862f0ab0aa9c06ecee
Leonardo Isso <leoisso.work@gmail.com>
2021-11-21 04:07:43 -0300

    feat: remove required fields validation

--- packages/server/src/domains/error/rules.js
+++ packages/server/src/domains/error/rules.js
@@ -3,8 +3,6 @@ import validations from './validations'
 const rules = {
   defaultCreateValidations: error => {
     try {
-      validations.missingPasswordFieldError(error)
-      validations.missingRequiredFieldError(error)
       validations.duplicateConstraintError(error)
       return error
     } catch (error) {
