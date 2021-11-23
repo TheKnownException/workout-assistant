7d24a8ee89c246a3a3c34c85c2c2b2338c61b56f
Leonardo Isso <leoisso.work@gmail.com>
2021-11-23 01:05:57 -0300

    feat: add password and model

--- packages/server/src/domains/error/constants.js
+++ packages/server/src/domains/error/constants.js
@@ -2,11 +2,21 @@ const EMAIL = {
   ALREADY_EXISTS: 'Email already exists'
 }
 
+const PASSWORD = {
+  INVALID: 'Password is invalid'
+}
+
 const FIELD = {
   IS_REQUIRED: field => `${field} is required`
 }
 
+const MODEL = {
+  NOT_FOUND: model => `${model} not found`
+}
+
 export default {
   EMAIL,
-  FIELD
+  FIELD,
+  MODEL,
+  PASSWORD
 }
