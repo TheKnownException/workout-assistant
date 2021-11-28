e006c5a617704e122e53f76645fbb9a1f4dd7d05
Leonardo Isso <leoisso.work@gmail.com>
2021-11-28 04:52:20 -0300

    fix: check for token validity

--- packages/server/src/infra/middlewares/gate.js
+++ packages/server/src/infra/middlewares/gate.js
@@ -3,20 +3,22 @@ import getOne from '../../views/get-one/get-one'
 
 const gate = async (req, res, next) => {
   const authorization = req.headers.authorization
-
   if (!authorization) {
     return res.status(401).json({ error: 'No authorization header' })
   }
 
   const token = user.rules.getTokenFromAuthorizationHeader(authorization)
-  const currentUser = await getOne(user, { token })
+  if (!token) {
+    return res.status(401).json({ error: 'Invalid token' })
+  }
 
+  const currentUser = await getOne(user, { token })
   if (!currentUser) {
     return res.status(401).json({ error: 'Invalid token' })
   }
 
-  req.locals = req.locals
-    ? { ...req.locals, user: currentUser }
+  res.locals = res.locals
+    ? { ...res.locals, user: currentUser }
     : { user: currentUser }
   next()
 }
