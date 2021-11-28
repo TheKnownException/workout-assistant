1ac722d2ed5079ff2ae0b104159d8627a6a4c189
Leonardo Isso <leoisso.work@gmail.com>
2021-11-28 03:51:20 -0300

    feat: add gate middleware

---
+++ packages/server/src/infra/middlewares/gate.js
@@ -0,0 +1,22 @@
+import user from '../../domains/user'
+import getOne from '../../views/get-one/get-one'
+
+const gate = async (req, res, next) => {
+  const authorization = req.headers('authorization')
+
+  if (!authorization) {
+    return res.status(401).json({ error: 'No authorization header' })
+  }
+
+  const token = user.rules.getTokenFromAuthorizationHeader(authorization)
+  const currentUser = await getOne(user, { token })
+
+  if (!currentUser) {
+    return res.status(401).json({ error: 'Invalid token' })
+  }
+
+  res.locals.user = currentUser
+  next()
+}
+
+export default gate
