53d9e8d0fd5115ecfa116c998c88d90fedd524f9
Leonardo Isso <leoisso.work@gmail.com>
2021-12-03 18:02:55 -0300

    feat: add createResetEmailData

--- packages/server/src/domains/password-reset-token/rules.js
+++ packages/server/src/domains/password-reset-token/rules.js
@@ -3,6 +3,15 @@ import crypto from 'crypto'
 const rules = {
   createPasswordResetToken: () => {
     return crypto.randomBytes(20).toString('hex')
+  },
+
+  createResetEmailData: (token, email) => {
+    return {
+      to: email,
+      subject: 'Workout Assistant - Reset your password',
+      html: `<p>You requested a password RESET at Workout Assistant</p>
+      Click <a href="${process.env.FRONTEND_URL}/reset-password/${token}">here</a> to reset your password.</p>`
+    }
   }
 }
 
