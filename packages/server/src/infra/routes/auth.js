a2b4bb43b3fd3577363dc3f98b74b11c4b6c4ba8
Leonardo Isso <leoisso.work@gmail.com>
2021-12-03 21:07:06 -0300

    feat: add put recover-password

--- packages/server/src/infra/routes/auth.js
+++ packages/server/src/infra/routes/auth.js
@@ -1,5 +1,6 @@
 import login from '../../views/login'
 import createPasswordResetToken from '../../views/create-password-reset-token/create-password-reset-token'
+import updateUserPassword from '../../views/update-user-password/update-user-password'
 
 export default app => {
   app.post('/login', async (request, response) => {
@@ -14,6 +15,16 @@ export default app => {
   app.post('/recover-password', async (request, response) => {
     const { body } = request
     const data = await createPasswordResetToken(body)
+    if (data.error) {
+      return response.status(400).json(data)
+    }
+    response.status(201).json(data)
+  })
+
+  app.put('/recover-password', async (request, response) => {
+    const { body } = request
+    const data = await updateUserPassword(body)
+    console.log(data)
     if (data.error) {
       return response.status(400).json(data)
     }
